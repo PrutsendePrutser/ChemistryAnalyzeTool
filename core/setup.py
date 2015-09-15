288d4ce079e06872e94513b51711d87c6882040b
PrutsendePrutser <bmtc.aarts@gmail.com>
2015-09-15 12:57:51 +0200

    Add setup-script for py2exe
    Added a setup script for py2exe so we can build the executable

---
+++ core/setup.py
@@ -0,0 +1,32 @@
+'''
+Created on 15 sep. 2015
+
+@author: Brian
+'''
+
+from distutils.core import setup
+import py2exe
+import matplotlib
+
+setup(console=["main.py"],
+      options={
+               'py2exe': {
+                          'packages' :  ['matplotlib', 'pytz'],
+                          'dll_excludes': ['libgdk-win32-2.0-0.dll',
+                                         'libgobject-2.0-0.dll',
+                                         'libgdk_pixbuf-2.0-0.dll',
+                                         'libgtk-win32-2.0-0.dll',
+                                         'libglib-2.0-0.dll',
+                                         'libcairo-2.dll',
+                                         'libpango-1.0-0.dll',
+                                         'libpangowin32-1.0-0.dll',
+                                         'libpangocairo-1.0-0.dll',
+                                         'libglade-2.0-0.dll',
+                                         'libgmodule-2.0-0.dll',
+                                         'libgthread-2.0-0.dll',
+                                         'QtGui4.dll', 'QtCore.dll',
+                                         'QtCore4.dll'
+                                        ],
+                          }
+                },
+      data_files=matplotlib.get_py2exe_datafiles(),)
