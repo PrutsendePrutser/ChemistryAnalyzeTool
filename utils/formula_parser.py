c83d52de433d1c7caaea2238533ad5738ebd0cfb
PrutsendePrutser <bmtc.aarts@gmail.com>
2015-09-24 15:06:49 +0200

    Add documentation to formula parser

--- utils/formula_parser.py
+++ utils/formula_parser.py
@@ -1,5 +1,8 @@
 from math import *
 
 def transform(formula, x=None, y=None, z=None):
+    # Apply the current formula using the values that were passed in
     value = eval(formula)
+    
+    # Return the value
     return value
